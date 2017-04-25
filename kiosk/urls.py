644cabffb8adffdb978ea83efa70e0d91b8b7c9f
Mark Faldborg <mark@faldb.org>
2017-04-25 22:40:50 +0200

    Added media root to the routing.

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.kiosk),
     url(r'^random$', views.find_random_image),
-]
-
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
