c231a849c1cb56ce53a071c5b26d5c280dfdc3a6
Troels Krøgh <troels.kroegh@gmail.com>
2017-09-07 16:56:02 +0200

    Add active as filter for product

--- stregsystem/admin.py
+++ stregsystem/admin.py
@@ -72,7 +72,7 @@ def toggle_active_selected_products(modeladmin, request, queryset):
 
 class ProductAdmin(admin.ModelAdmin):
     search_fields = ('name', 'price', 'id')
-    list_filter = ('deactivate_date', 'price')
+    list_filter = ('active', 'deactivate_date', 'price')
     list_display = ('activated', 'id', 'name', 'get_price_display')
     actions = [toggle_active_selected_products]
 
