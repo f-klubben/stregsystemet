cc68a8c602999644380e0eb315db46b8f40c06b7
Troels Krøgh <troels.kroegh@gmail.com>
2017-09-13 15:19:57 +0200

    Changed kiosk to be deterministic and corrected for python3

--- kiosk/admin.py
+++ kiosk/admin.py
@@ -1,21 +1,29 @@
 from django.contrib import admin
 
-from models import KioskItem
+from .models import KioskItem
+
 
 def set_active_kiosk_item(modeladmin, request, queryset):
     queryset.update(active=True)
+
+
 set_active_kiosk_item.short_description = "Make selected kiosk items active"
+
 
 def set_inactive_kiosk_item(modeladmin, request, queryset):
     queryset.update(active=False)
+
+
 set_inactive_kiosk_item.short_description = "Make selected kiosk items inactive"
 
+
 class KioskItemAdmin(admin.ModelAdmin):
-    search_fields = ('name', )
+    search_fields = ('name',)
     list_display = ('name', 'active')
-    list_filter = ('active', )
+    list_filter = ('active',)
     list_display_links = ('name', 'active')
     actions = [set_active_kiosk_item, set_inactive_kiosk_item]
+
 
 # Register your models here.
 admin.site.register(KioskItem, KioskItemAdmin)
--- kiosk/views.py
+++ kiosk/views.py
@@ -1,16 +1,51 @@
+import time
+from datetime import datetime
+
+from django.http import Http404
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
 
-from models import KioskItem
+from .models import KioskItem
 
-def show_pics(request):
-    items = list(KioskItem.objects.all())
-    return render(request, 'allkioskitems.html', locals())
 
 def kiosk(request):
     return render(request, 'kiosk.html', locals())
 
+
 def find_random_image(request):
+    """
+    Randomly get an image and return the relative url
+    """
     item = KioskItem.objects.filter(active=True).order_by('?').first()
+    if item is None:
+        raise Http404("No active kiosk items found")
+
     return HttpResponse(item.image.url, content_type="text/plain")
 
+
+def find_next_image(request):
+    """
+    Deterministically return an image for the kiosk screen.
+    This ensures that each kiosk item will be displayed an equal amount of time,
+    assuming the screen call this method semi-frequently.
+
+    :param request: Django request obj
+    :return: The request or 404 if no active kiosk items was found.
+    """
+    item_count = KioskItem.objects.filter(active=True).count()
+    if item_count == 0:
+        raise Http404("No active kiosk items found")
+
+    # The duration for each item
+    # TODO: Make this a variable for someone to adjust (Where?)
+    duration = 10
+    # The total cycle time
+    complete_cycle_time = duration * item_count
+    # Get a unit timestamp
+    seconds_since_epoch = time.mktime(datetime.now().timetuple())
+    # Find the next index, by getting a number in the range [0;complete_cycle_time[
+    # and divide it by the duration of each
+    next_index = int((seconds_since_epoch % complete_cycle_time) / duration)
+    # Get the item at the index, trust that Django does this smartly.
+    next_item = KioskItem.objects.filter(active=True).order_by('name')[next_index]
+    return HttpResponse(next_item.image.url, content_type="text/plain")
