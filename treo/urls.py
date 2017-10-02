132c97b5d599a3db2cb54bdeca3894157770c104
Troels Krøgh <troels.kroegh@gmail.com>
2017-10-02 12:05:13 +0200

    Add django-debug-toolbar

--- treo/urls.py
+++ treo/urls.py
@@ -29,3 +29,9 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
