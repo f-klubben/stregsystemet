237943c58ef71823e13d4c8756ea5c31cc0d0826
Mathias Jakobsen <mathias@jbcoding.com>
2018-02-15 10:05:22 +0100

    Remove delete button from tab-order

--- stregsystem/static/stregsystem/jquery.formset.js
+++ stregsystem/static/stregsystem/jquery.formset.js
@@ -85,15 +85,15 @@
 			if (row.is('TR')) {
 				// If the forms are laid out in table rows, insert
 				// the remove button into the last table cell:
-				row.children(':last').append('<a class="' + options.deleteCssClass +'" href="javascript:void(0)">' + options.deleteText + '</a>');
+				row.children(':last').append('<a tabindex="-1" class="' + options.deleteCssClass +'" href="javascript:void(0)">' + options.deleteText + '</a>');
 			} else if (row.is('UL') || row.is('OL')) {
 				// If they're laid out as an ordered/unordered list,
 				// insert an <li> after the last list item:
-				row.append('<li><a class="' + options.deleteCssClass + '" href="javascript:void(0)">' + options.deleteText +'</a></li>');
+				row.append('<li><a tabindex="-1" class="' + options.deleteCssClass + '" href="javascript:void(0)">' + options.deleteText +'</a></li>');
 			} else {
 				// Otherwise, just insert the remove button as the
 				// last child element of the form's container:
-				row.append('<a class="' + options.deleteCssClass + '" href="javascript:void(0)">' + options.deleteText +'</a>');
+				row.append('<a tabindex="-1" class="' + options.deleteCssClass + '" href="javascript:void(0)">' + options.deleteText +'</a>');
 			}
 			// Check if we're under the minimum number of forms - not to display delete link at rendering
 			if (!canDeleteRow()){
