460565d73e054bba2a3098b1bcf7e5b6112b3eb4
Mathias Jakobsen <mathias@jbcoding.com>
2020-02-11 14:18:19 +0100

    Hide all usernames in gdpr.js

--- stregsystem/static/stregsystem/gdpr.js
+++ stregsystem/static/stregsystem/gdpr.js
@@ -3,14 +3,13 @@ Hides the username after 5 seconds of showing it.
 Simply replaces the letters with 'x'.
 */
 
-var username_element = document.getElementById("username");
-if (username_element !== null) {
+var username_element = document.querySelectorAll(".username");
 
+if (username_element.length > 0) {
   setTimeout(function () {
-
-    var username_element = document.getElementById("username");
-    if (username_element !== null) {
-      username_element.innerText = "xxxxxxxx";
+    var username_element = document.querySelectorAll(".username");
+    for (var i = 0; i < username_element.length; i++) {
+    	username_element[i].innerText = "xxxxxxxx";
     }
   }, 5000);
 }
