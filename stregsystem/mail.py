c79a4f2027a20eda7a555d7816ee967b3575bf19
Mathias Jakobsen <mathias@jbcoding.com>
2020-02-11 11:03:22 +0100

    Add english version of welcome-email

--- stregsystem/mail.py
+++ stregsystem/mail.py
@@ -36,6 +36,21 @@ def send_welcome_mail(member):
 			Følg med på <a href="https://www.facebook.com/fklub">Facebook</a> og <a href="https://www.instagram.com/fklub">Instagram</a> for events, billeder, og andre relevante indslag.<br><br>
 			Husk at der er fredagsfranskbrød i kantinen hver onsdag kl. 10.00!<br><br> 
             Med venlig hilsen,<br>
+            F-klubben <br><br>
+
+            ====================================== <br><br>
+
+            Hi {member.firstname}!<br><br>
+            Welcome as a fember (member) of F-klubben!
+            Your stregkonto (account) has been created with the following username: <b>{member.username}</b>.<br>
+            From now on you can use your username in <a href="http://fklub.dk/treo/stregsystem">stregsystemet</a> to purchase goods and/or event tickets, if you have funds on your account.<br>
+            Your current balance is: {money(member.balance)} kr.<br><br>
+            If you have any questions please take a look at <a href="http://fklub.dk">fklub.dk</a>, but otherwise contact us at <a href="mailto:info@fklub.dk">info@fklub.dk</a> or <a href="https://www.facebook.com/fklub">Facebook</a>.<br><br>
+
+            Follow us on <a href="https://www.facebook.com/fklub">Facebook</a> and <a href="https://www.instagram.com/fklub">Instagram</a> for events, pictures and other relevant postings.<br><br>
+            Remember that there is fredagsfranskbrød (friday bread) in the canteen every Wednesday at 10.00!<br><br>
+
+            Best regards,<br>
             F-klubben
         </body>
     </html>
