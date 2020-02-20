930225f425e6a554c301a8eaa180721965367073
cogitantium <falke.cs@gmail.com>
2020-02-20 18:00:14 +0100

    [username_hider] Add April Fools username easter egg

--- stregsystem/static/stregsystem/gdpr.js
+++ stregsystem/static/stregsystem/gdpr.js
@@ -6,10 +6,17 @@ Simply replaces the letters with 'x'.
 var username_element = document.querySelectorAll(".username");
 
 if (username_element.length > 0) {
-  setTimeout(function () {
-    var username_element = document.querySelectorAll(".username");
-    for (var i = 0; i < username_element.length; i++) {
-    	username_element[i].innerText = "xxxxxxxx";
-    }
-  }, 5000);
+    setTimeout(function () {
+        var username_element = document.querySelectorAll(".username");
+        var replaced_username = "[username_hidden]";
+        var date = new Date();
+        // Only do username memes on April Fools
+        if (date.getDate() === 1 && date.getMonth() + 1 === 4) {
+            const easter_egg_names = ["user", "admin", "root", "alan_turing", "hackerman", "hans_huttel"];
+            replaced_username = easter_egg_names[Math.floor(Math.random() * easter_egg_names.length)];
+        }
+        for (var i = 0; i < username_element.length; i++) {
+            username_element[i].innerText = replaced_username;
+        }
+    }, 5000);
 }
