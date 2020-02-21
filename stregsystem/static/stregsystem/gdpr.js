b41abee1b608bf3579a3579dc57e68ff7865689c
cogitantium <falke.cs@gmail.com>
2020-02-21 15:00:44 +0100

    [username_hider] Fix date conditional for replacements to be on Fridays

--- stregsystem/static/stregsystem/gdpr.js
+++ stregsystem/static/stregsystem/gdpr.js
@@ -10,8 +10,8 @@ if (username_element.length > 0) {
         var username_element = document.querySelectorAll(".username");
         var replaced_username = "[username_hidden]";
         var date = new Date();
-        // Only do username memes on April Fools
-        if (date.getDate() === 21 && date.getMonth() + 1 === 2) {
+        // Only do username replacements on Fridays
+        if (date.getDay() === 5) {
             const easter_egg_names = ["user", "admin", "root", "alan_turing", "hackerman", "foo", "bar"];
             replaced_username = easter_egg_names[Math.floor(Math.random() * easter_egg_names.length)];
         }
