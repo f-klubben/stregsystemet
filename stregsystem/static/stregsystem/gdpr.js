71b767cee8d84f1cfaefb74722d465c1b6aa04b3
cogitantium <falke.cs@gmail.com>
2020-02-25 11:51:34 +0100

    Revert "[username_hider] Fix random function"
    This reverts commit 6c9171d4d291385929c592a04955b4593e564ed0.

--- stregsystem/static/stregsystem/gdpr.js
+++ stregsystem/static/stregsystem/gdpr.js
@@ -9,14 +9,14 @@ if (username_element.length > 0) {
     setTimeout(function () {
         var username_element = document.querySelectorAll(".username");
         var replaced_username = "[username_hidden]";
+        var date = new Date();
         // Only do username replacements on Fridays
-        if (new Date().getDay() === 5) {
+        if (date.getDay() === 5) {
             const easter_egg_names = ["user", "admin", "root", "alan_turing", "dijkstra", "knuth", "hackerman", "foo",
                 "bar", "Robert\'); DROP TABLE fembers;--", "hunter2", "correcthorsebatterystaple",
                 String.fromCodePoint(0x1F4BE), String.fromCodePoint(0x1F37A),
                 String.fromCodePoint(0x1F937)];
-            // Adding by one as Math.random returns a range [0..1)
-            replaced_username = easter_egg_names[Math.floor(Math.random() * easter_egg_names.length + 1)];
+            replaced_username = easter_egg_names[Math.floor(Math.random() * easter_egg_names.length)];
         }
         for (var i = 0; i < username_element.length; i++) {
             username_element[i].innerText = replaced_username;
