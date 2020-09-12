97f093e154eee698b9bde02448cd1af6b28fd9cf
cogitantium <falke.cs@gmail.com>
2020-09-12 18:51:04 +0200

    [paytool] Add path fix of script and auth user for fixture

--- reload-django-test.sh
+++ reload-django-test.sh
@@ -3,5 +3,5 @@
 rm db.sqlite3
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata stregsystem/fixtures/testdata.json
+python manage.py loaddata stregsystem/fixtures/testdata-mobilepay.json
 python manage.py runserver
