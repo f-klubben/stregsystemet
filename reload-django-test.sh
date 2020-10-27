07bbac8fff5ff00bbc095ae3da7865fb6a4c160d
cogitantium <falke.cs@gmail.com>
2020-10-27 17:32:10 +0100

    Remove Mobilepay Django reload script

--- reload-django-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm db.sqlite3
-python manage.py makemigrations
-python manage.py migrate
-python manage.py loaddata stregsystem/fixtures/testdata-mobilepay.json
-python manage.py runserver
