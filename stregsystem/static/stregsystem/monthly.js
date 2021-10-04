cc8d0888e4657b2063ded4ff9d371ee35bfad98e
MKKlausen <mathias.karstoft.klausen@gmail.com>
2021-10-04 20:25:56 +0200

    Tried to make the code slightly more readable.

--- stregsystem/static/stregsystem/monthly.js
+++ stregsystem/static/stregsystem/monthly.js
@@ -6,43 +6,43 @@ var MONTHLYSPRITE = {
 }
 
 spawn_sprite = function() {
-    var v = document.createElement('video');
-    var s = document.createElement('source');
+    var video_element = document.createElement('video');
+    var video_source = document.createElement('source');
     var z = document.createElement('div');
-    var zs = z.style;
-    var a = window.innerWidth * Math.random();
-    var b = window.innerHeight * Math.random();
+    var z_style = z.style;
+    var scaled_width = window.innerWidth * Math.random();
+    var scaled_height = window.innerHeight * Math.random();
     z.classList.add("monthly-sprite");
-    z.appendChild(v);
-    v.appendChild(s);
-    v.width = "48";
-    v.height = "48";
-    v.autoplay = true;
-    v.loop = true;
-    v.muted = true;
-    s.src = media_url + "stregsystem/" +MONTHLYSPRITE[d.getMonth()];
-    s.type = "video/webm";
+    z.appendChild(video_element);
+    video_element.appendChild(video_source);
+    video_element.width = "48";
+    video_element.height = "48";
+    video_element.autoplay = true;
+    video_element.loop = true;
+    video_element.muted = true;
+    video_source.src = media_url + "stregsystem/" +MONTHLYSPRITE[d.getMonth()];
+    video_source.type = "video/webm";
     document.body.querySelector(".monthly-sprite-container").appendChild(z);
 
-    function R(o, m) {
-        return Math.max(Math.min(o + (Math.random() - 0.5) * 400, m - 50), 50);
+    function animation_start_location(scaled_dimension, window_dimension) {
+        return Math.max(Math.min(scaled_dimension + (Math.random() - 0.5) * 400, window_dimension - 50), 50);
     }
 
-    function A(){
-        var x = R(a, window.innerWidth);
-        var y = R(b, window.innerHeight);
-        var d = Math.round(10 * Math.sqrt((a - x) * (a - x) + (b - y) * (b - y)));
-        zs.opacity = 1;
-        zs.transitionDuration = zs.transitionDuration = d + 'ms';
+    function sprite_animation(){
+        var x = animation_start_location(scaled_width, window.innerWidth);
+        var y = animation_start_location(scaled_height, window.innerHeight);
+        var animation_duration = Math.round(10 * Math.sqrt((scaled_width - x) * (scaled_width - x) + (scaled_height - y) * (scaled_height - y)));
+        z_style.opacity = 1;
+        z_style.transitionDuration = z_style.transitionDuration = animation_duration + 'ms';
         // zs.transform = zs.webkitTransform = 'translate(' + x + 'px, ' + y + 'px)';
-        zs.left = x + 'px';
-        zs.top = y + 'px';
-        v.style.transform = v.style.transform = (a > x) ? '' : 'scaleX(-1)';
-        a = x;
-        b = y;
-        setTimeout(A,d);
+        z_style.left = x + 'px';
+        z_style.top = y + 'px';
+        video_element.style.transform = video_element.style.transform = (scaled_width > x) ? '' : 'scaleX(-1)';
+        scaled_width = x;
+        scaled_height = y;
+        setTimeout(sprite_animation,animation_duration);
     }
-    setTimeout(A, Math.random() * 3e3);
+    setTimeout(sprite_animation, Math.random() * 3e3);
 };
 
 d = new Date();
