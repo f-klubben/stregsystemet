cc932441463ccc4aaffbc93ab458b48aef13998c
Simon Nielsen <Simonhelmingnielsen@gmail.com>
2021-11-24 20:52:31 +0100

    christmas

--- stregsystem/static/stregsystem/snow.js
+++ stregsystem/static/stregsystem/snow.js
@@ -1,8 +1,7 @@
 spawn_snowflake = () => {
-
-    const z = document.createElement('div');
-    z.classList.add("snowflake");
-    document.body.querySelector(".snow-container").appendChild(z);
+    const snowflake = document.createElement('div');
+    snowflake.classList.add("snowflake");
+    document.body.querySelector(".snow-container").appendChild(snowflake);
 }
 
 d = new Date();
@@ -10,9 +9,11 @@ if(d.getMonth() === 10){
     for(let n_snowflakes=0; n_snowflakes < d.getDate(); n_snowflakes++){
         spawn_snowflake();
     }
-    let z = document.createElement('div');
-    let gif = document.createElement("img")
-    gif.src="https://www.animatedimages.org/data/media/359/animated-santa-claus-image-0420.gif"
-    z.classList.add("santa");
+    const santa = document.createElement('div');
+    santa.classList.add("santa");
+    const gif = document.createElement("img")
+    gif.src="https://www.animatedimages.org/data/media/359/animated-santa-claus-image-0420.gif";
+    santa.appendChild(gif);
+    document.body.querySelector(".snow-container").appendChild(santa);
 }
 
