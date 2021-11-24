819dad0514aefb78c4376adddfb3217c31ddcc48
Simon Nielsen <Simonhelmingnielsen@gmail.com>
2021-11-24 20:06:44 +0100

    christmas themed UI

--- stregsystem/static/stregsystem/snow.js
+++ stregsystem/static/stregsystem/snow.js
@@ -1,6 +1,6 @@
 spawn_snowflake = () => {
 
-    var z = document.createElement('div');
+    const z = document.createElement('div');
     z.classList.add("snowflake");
     document.body.querySelector(".snow-container").appendChild(z);
 }
@@ -10,5 +10,9 @@ if(d.getMonth() === 10){
     for(let n_snowflakes=0; n_snowflakes < d.getDate(); n_snowflakes++){
         spawn_snowflake();
     }
+    let z = document.createElement('div');
+    let gif = document.createElement("img")
+    gif.src="https://www.animatedimages.org/data/media/359/animated-santa-claus-image-0420.gif"
+    z.classList.add("santa");
 }
 
