9e816dbb1040d48d2a6b523a0b0331cdb0155fc3
Kresten Laust <krestenlaust@gmail.com>
2023-08-16 20:30:13 +0200

    Refactored mail functions to reduce code duplication

--- stregsystem/mail.py
+++ stregsystem/mail.py
@@ -12,35 +12,16 @@ from stregsystem.templatetags.stregsystem_extras import money
 logger = logging.getLogger(__name__)
 
 
-def send_email(mailadress, msg_string):
-    if hasattr(settings, 'TEST_MODE'):
-        return
-    try:
-        smtpObj = smtplib.SMTP('localhost', 25)
-        smtpObj.sendmail('treo@fklub.dk', mailadress, msg_string)
-    except Exception as e:
-        logger.error(str(e))
-
-
 def send_welcome_mail(member):
-    msg = MIMEMultipart()
-
     context = dict()
     context.update(vars(member))
     context.update({'formatted_balance': money(member.balance)})
 
-    html = render_to_string("mail/welcome.html", context)
-
-    msg.attach(MIMEText(html, 'html'))
-    send_email(member.email, msg.as_string())
+    # Original code didn't specify a subject line.
+    send_template_mail(member, "welcome.html", context, None)
 
 
 def send_payment_mail(member, amount, mobilepay_comment):
-    msg = MIMEMultipart()
-    msg['From'] = 'treo@fklub.dk'
-    msg['To'] = member.email
-    msg['Subject'] = 'Stregsystem payment'
-
     context = dict()
     context.update(vars(member))
     context.update({'formatted_amount': money(amount)})
@@ -48,7 +29,21 @@ def send_payment_mail(member, amount, mobilepay_comment):
     context.update({'mobilepay_comment': escape(mobilepay_comment)})
 
     target_template = "deposit_manual.html" if mobilepay_comment else "deposit_automatic.html"
-    html = render_to_string(f"mail/{target_template}", context)
+    send_template_mail(member, target_template, context, "Stregsystem payment")
 
+
+def send_template_mail(member, target_template: str, context: dict, subject: str):
+    msg = MIMEMultipart()
+    msg['From'] = 'treo@fklub.dk'
+    msg['To'] = member.email
+    msg['Subject'] = subject
+    html = render_to_string(f"mail/{target_template}", context)
     msg.attach(MIMEText(html, 'html'))
-    send_email(member.email, msg.as_string())
+
+    if hasattr(settings, 'TEST_MODE'):
+        return
+    try:
+        smtpObj = smtplib.SMTP('localhost', 25)
+        smtpObj.sendmail('treo@fklub.dk', member.email, msg.as_string())
+    except Exception as e:
+        logger.error(str(e))
