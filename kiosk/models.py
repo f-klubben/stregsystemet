0bea0173ba04ec0a4a1c12171e60b90bddd38a20
Kresten Laust <krestenlaust@gmail.com>
2023-08-18 13:43:16 +0200

    Added start_datetime and end_datetime fields to KioskItem, and added conditions for filtering.

--- kiosk/models.py
+++ kiosk/models.py
@@ -1,4 +1,5 @@
 import random
+from django.utils import timezone
 from .validators import validate_file_extension, valid_images
 from django.db import models
 import os
@@ -15,8 +16,13 @@ class KioskItem(models.Model):
     active = models.BooleanField(default=True)
     media = models.FileField(upload_to='kiosk', null=False, validators=[validate_file_extension])
     ordering = models.IntegerField(null=False, default=random_ordering, blank=False)
+    start_datetime = models.DateTimeField(default=timezone.now(), blank=True)
+    end_datetime = models.DateTimeField(null=True, blank=True)
 
     @property
     def is_image(self):
         name, extension = os.path.splitext(self.media.name)
         return extension in valid_images
+
+    def is_active_period(self, current_time) -> bool:
+        return (self.start_datetime < current_time) and (self.end_datetime is None or self.end_datetime > current_time)
--- kiosk/views.py
+++ kiosk/views.py
@@ -1,5 +1,7 @@
 import json
 
+from datetime import datetime
+from django.utils import timezone
 from django.db.models import Q
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
@@ -15,7 +17,8 @@ def find_random_media(request):
     """
     Randomly get a media and return the relative url
     """
-    item = KioskItem.objects.filter(active=True).order_by('?').first()
+    item = KioskItem.objects.filter(active=True, start_datetime__lte=timezone.now())\
+        .filter(Q(end_datetime__isnull=True) | Q(end_datetime__gt=timezone.now())).order_by('?').first()
     if item is None:
         raise Http404("No active kiosk items found")
 
@@ -30,19 +33,22 @@ def find_random_media(request):
 def find_next_media_real(request, item_id):
     item = KioskItem.objects.get(pk=item_id)
 
-    item_count = KioskItem.objects.filter(active=True).count()
+    item_count = KioskItem.objects.filter(active=True, start_datetime__lte=timezone.now())\
+        .filter(Q(end_datetime__isnull=True) | Q(end_datetime__gt=timezone.now())).count()
     if item_count == 0:
         raise Http404("No active kiosk items found")
 
     # Get the item at the index, trust that Django does this smartly.
     try:
         next_item = (
-            KioskItem.objects.filter(active=True)
+            KioskItem.objects.filter(active=True, start_datetime__lte=timezone.now())
+            .filter(Q(end_datetime__isnull=True) | Q(end_datetime__gt=timezone.now()))
             .order_by('ordering', 'id')
             .filter(Q(ordering__gt=item.ordering) | (Q(ordering=item.ordering) & Q(id__gt=item.id)))[0]
         )
     except IndexError:
-        next_item = KioskItem.objects.filter(active=True).order_by('ordering', 'id')[0]
+        next_item = KioskItem.objects.filter(active=True, start_datetime__lte=timezone.now())\
+            .filter(Q(end_datetime__isnull=True) | Q(end_datetime__gt=timezone.now())).order_by('ordering', 'id')[0]
     response_data = {
         "id": next_item.id,
         "url": next_item.media.url,
