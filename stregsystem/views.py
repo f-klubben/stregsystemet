2bb08bef6f1c9f346a926f3219e70cb98a164a75
Kresten Laust <krestenlaust@gmail.com>
2023-09-05 20:09:43 +0200

    Finished implementing database lookup! Basic heatmap functionality is finished.

--- stregsystem/views.py
+++ stregsystem/views.py
@@ -87,21 +87,25 @@ def __organize_purchase_heatmap_data(heatmap_data: list, start_date: datetime.da
 
 def __get_purchase_heatmap_day_color(
     products: List[Product],
-    category_name_color: (
-        str,
-        str,
-        str,
+    products_by_color: (
+        List[Product],
+        List[Product],
+        List[Product],
     ),
 ) -> (int, int, int,):
-    red = 0
-    green = 0
-    blue = 0
+    category_representation = [0, 0, 0]
 
     for product in products:
-        print(product.categories)
-        pass
+        for category_products_index in range(3):
+            if product in products_by_color[category_products_index]:
+                category_representation[category_products_index] = category_representation[category_products_index] + 1
 
-    return (50, 50, 50)
+    total_category_sum = sum(category_representation)
+
+    if total_category_sum == 0:
+        return 235, 237, 240  # Grey
+
+    return tuple(category_sum / total_category_sum * 255 for category_sum in category_representation)
 
 
 def __get_purchase_heatmap_data(
@@ -116,11 +120,10 @@ def __get_purchase_heatmap_data(
 ) -> list:
     from datetime import timedelta
 
-    # Possibly off-by-one because visual starts on sunday, but weekday property has sunday be 6.
     days_to_go_back = (7 * weeks_to_display) - (6 - end_date.weekday() - 1)
     cutoff_date = end_date.date() - timedelta(days=days_to_go_back)
     last_sale_list = iter(
-        member.sale_set.filter(timestamp__gte=cutoff_date, timestamp__lt=end_date).order_by('timestamp')
+        member.sale_set.filter(timestamp__gte=cutoff_date, timestamp__lte=end_date).order_by('timestamp')
     )
 
     products_by_day = []
@@ -149,18 +152,24 @@ def __get_purchase_heatmap_data(
             next_sale_date = None
             continue
 
+    category_by_name = tuple(Category.objects.filter(name=cat_name) for cat_name in category_name_color)
+    products_by_category = tuple(
+        Product.objects.filter(categories__in=category_ins) for category_ins in category_by_name
+    )
+
     # Proposed format:
     # Returned list: [<<Day 0 (today)>>, <<Day 1 (yesterday)>>, ...]
     # Day item: (<<Date>>, <<RGB values tuple-format>>, [<<item ID 1>>, ...])
     days = []
 
     for day_index in range(len(products_by_day)):
-        day_color = __get_purchase_heatmap_day_color(products_by_day[day_index], category_name_color)
+        day_color = __get_purchase_heatmap_day_color(products_by_day[day_index], products_by_category)
         days.append((dates_by_day[day_index], day_color, [x.id for x in products_by_day[day_index]]))
 
     return days
 
 
+"""
 def __get_purchase_heatmap_data_mockup(
     member: Member,
     end_date: datetime.datetime,
@@ -188,7 +197,7 @@ def __get_purchase_heatmap_data_mockup(
             )
         )
 
-    return mockup_list
+    return mockup_list"""
 
 
 def roomindex(request):
