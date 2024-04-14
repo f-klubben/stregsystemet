0bd4e361f1bfd84751c03bc1d63fe2aaa68c2b25
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2024-04-14 15:14:21 +0200

    Created reimbursement model

--- stregsystem/models.py
+++ stregsystem/models.py
@@ -22,6 +22,8 @@ from stregsystem.utils import (
 )
 from stregsystem.mail import send_payment_mail
 
+MAX_REIMBUSEMENT_HOURS = 12
+
 
 def price_display(value):
     return money(value) + " kr."
@@ -40,6 +42,9 @@ class StregForbudError(Exception):
 
 
 class NoMoreInventoryError(Exception):
+    pass
+
+class SaleNotFoundError(Exception):
     pass
 
 
@@ -189,18 +194,18 @@ class Member(models.Model):  # id automatisk...
 
     def __str__(self):
         return (
-            active_str(self.active)
-            + " "
-            + self.username
-            + ": "
-            + self.firstname
-            + " "
-            + self.lastname
-            + " | "
-            + self.email
-            + " ("
-            + money(self.balance)
-            + ")"
+                active_str(self.active)
+                + " "
+                + self.username
+                + ": "
+                + self.firstname
+                + " "
+                + self.lastname
+                + " | "
+                + self.email
+                + " ("
+                + money(self.balance)
+                + ")"
         )
 
     # XXX - virker ikke
@@ -298,8 +303,8 @@ class Member(models.Model):  # id automatisk...
             [
                 Intake(x.timestamp, x.product.caffeine_content_mg)
                 for x in self.sale_set.filter(
-                    timestamp__gt=timezone.now() - CAFFEINE_TIME_INTERVAL, product__caffeine_content_mg__gt=0
-                ).order_by('timestamp')
+                timestamp__gt=timezone.now() - CAFFEINE_TIME_INTERVAL, product__caffeine_content_mg__gt=0
+            ).order_by('timestamp')
             ]
         )
 
@@ -638,6 +643,10 @@ class Sale(models.Model):
     # XXX - django bug - kan ikke vaelge mellem desc og asc i admin, som ved normalt felt
     price_display.admin_order_field = 'price'
 
+    def is_reimbursable(self):
+        from datetime import timedelta
+        return timedelta(hours=MAX_REIMBUSEMENT_HOURS) >= timezone.now() - self.timestamp
+
     @deprecated
     def __unicode__(self):
         return self.__str__()
@@ -673,3 +682,11 @@ class News(models.Model):
 
     def __str__(self):
         return self.title + " -- " + str(self.pub_date)
+
+
+class Reimbursement(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
+
