06fee8d59e83b47d065f414b0923796b272e87c2
Patrick Reiffenstein <patrick@reiffenste.in>
2024-06-12 19:35:59 +0200

    Removed duplicate queries

--- stregsystem/views.py
+++ stregsystem/views.py
@@ -715,14 +715,23 @@ def api_quicksale(request, room, member: Member, bought_ids):
 
 def __append_bought_ids_to_product_list(products, bought_ids, time_now, room):
     try:
-        for i in bought_ids:
+        # Get the amount of unique items bought
+        unique_product_dict = {}
+        for unique_id in bought_ids:
+            if str(unique_id) not in unique_product_dict:
+                unique_product_dict[str(unique_id)] = 1
+            else:
+                unique_product_dict[str(unique_id)] += 1
+
+        # Add the given amount of different products
+        for i in unique_product_dict:
             product = Product.objects.get(
                 Q(pk=i),
                 Q(active=True),
                 Q(deactivate_date__gte=time_now) | Q(deactivate_date__isnull=True),
                 Q(rooms__id=room.id) | Q(rooms=None),
             )
-            products.append(product)
+            products.extend([product for x in range(unique_product_dict[str(i)])])
     except Product.DoesNotExist:
         return "Invalid product id", 400, i
     return "OK", 200, None
