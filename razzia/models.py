1c7aeaaecf138397629d7848a7b02c532683a430
Kresten Laust <krestenlaust@gmail.com>
2024-09-14 15:15:16 +0200

    fixup! Add turns per member

--- razzia/models.py
+++ razzia/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from stregsystem.models import Member
+from datetime import timedelta
 
 
 class Razzia(models.Model):
@@ -9,7 +10,7 @@ class Razzia(models.Model):
 
     name = models.CharField(max_length=20)
     turns_per_member = models.IntegerField(default=0)
-    turn_interval = models.DurationField(default=0)
+    turn_interval = models.DurationField(default=timedelta(0))
     # required_products = models.ManyToManyField(Product)
     # purchase_start_date = models.DateTimeField(null=True, blank=True)
     # purchase_end_date = models.DateTimeField(null=True, blank=True)
