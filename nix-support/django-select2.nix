dc1dbccd0c477614305e2112373a7c3604848c66
Thomas Jensen <mast3r@skade.dev>
2024-09-25 16:47:32 +0200

    fixing some python dependencies

--- nix-support/django-select2.nix
+++ nix-support/django-select2.nix
@@ -2,18 +2,19 @@
 
 py.buildPythonPackage {
     pname = "Django-Select2";
-    version = "5.11.1";
+    version = "8.1.2";
     src = pkgs.fetchurl {
-        url = "https://files.pythonhosted.org/packages/a9/67/4a511634562a3108261d6497bd2d6e40af957b9d3d75f30ec95cc68ccf0b/Django-Select2-5.11.1.tar.gz";
-        sha256 = "06gb79wikwcwbsny6dz9vp8qv66k0ri8xfd0h3hay58rc7mkrg2a";
+        url = "https://files.pythonhosted.org/packages/7f/af/2fc371e1dea6686b588ab9cd445e55b63248a525f8c90550767a42dd77bb/django_select2-8.1.2.tar.gz";
+        sha256 = "sha256-9EaF7hw5CQqt4B4+vCVnAvBWIPPHijwmhECtmmYHCHY=";
     };
-    format = "setuptools";
+    format = "pyproject";
     doCheck = false;
     buildInputs = [];
     checkInputs = [];
     nativeBuildInputs = [];
     propagatedBuildInputs = [
-        (py.django-appconf)
+        py.django-appconf
+        py.flit-scm
     ];
 }
 
