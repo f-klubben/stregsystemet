dc7191078ba87bb6b6e51bc55c22a0a773dd9b03
Sofi <sofi+git@mailbox.org>
2024-10-08 14:49:39 +0200

    refactor: do not generate html in js

--- stregsystem/static/stregsystem/themes/activatefklub/activatefklub.js
+++ stregsystem/static/stregsystem/themes/activatefklub/activatefklub.js
@@ -1,26 +1,17 @@
 
-attempt_spawn_activatefklub()
+attempt_show_activatefklub()
 
-function attempt_spawn_activatefklub() {
+function attempt_show_activatefklub() {
     // Easter egg once a month inspired by "Activate Windows". Make the easter
     // egg show itself on each Patch Tuesday for Microsoft, which is placed on
     // every second Tuesday of each month.
     // https://en.wikipedia.org/wiki/Patch_Tuesday
     const now = new Date();
     const isSecondTuesday = now.getDay() === 2 && now.getDate() >= 8 && now.getDate() <= 14;
+    const container = document.getElementById("activatefklub-container");
+
     if (isSecondTuesday) {
-        spawn_activatefklub();
+        container.classList.add("activatefklub-shown");
         console.log("Hooray its Patch Tuesday! 🎉🪟");
     }
-}
-
-function spawn_activatefklub() {
-    const header = document.createElement("p");
-    header.textContent = "Activate Stregsystemet";
-    header.classList.add("activatefklub-header");
-    document.body.querySelector(".activatefklub-container").appendChild(header);
-
-    const description = document.createElement("p");
-    description.textContent = "Go to the Admin panel to activate Stregsystemet.";
-    document.body.querySelector(".activatefklub-container").appendChild(description);
 }
