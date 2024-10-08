63df4cc3007270b1c11c6727eb7162b2efeafbd3
Sofi <sofi+git@mailbox.org>
2024-10-08 13:50:42 +0200

    fix: make easteregg only show itself once a month

--- stregsystem/static/stregsystem/themes/activatefklub/activatefklub.js
+++ stregsystem/static/stregsystem/themes/activatefklub/activatefklub.js
@@ -2,9 +2,15 @@
 attempt_spawn_activatefklub()
 
 function attempt_spawn_activatefklub() {
-    // Only activate on patch tuesdays.
-    if (new Date().getDay() == 2) {
-        spawn_activatefklub()
+    // Easter egg once a month inspired by "Activate Windows". Make the easter
+    // egg show itself on each Patch Tuesday for Microsoft, which is placed on
+    // every second Tuesday of each month.
+    // https://en.wikipedia.org/wiki/Patch_Tuesday
+    const now = new Date();
+    const isSecondTuesday = now.getDay() === 2 && now.getDate() >= 8 && now.getDate() <= 14;
+    if (isSecondTuesday) {
+        spawn_activatefklub();
+        console.log("Hooray its Patch Tuesday! 🎉🪟");
     }
 }
 
