1b4e366e26b3623e23e872334ac3fd992c6c2839
Sofi <sofi+git@mailbox.org>
2024-10-08 13:21:26 +0200

    fix: change phrase, lighten font color

--- stregsystem/static/stregsystem/themes/activatefklub/activatefklub.js
+++ stregsystem/static/stregsystem/themes/activatefklub/activatefklub.js
@@ -10,11 +10,11 @@ function attempt_spawn_activatefklub() {
 
 function spawn_activatefklub() {
     const header = document.createElement("p");
-    header.textContent = "Activate your Fklub Account";
+    header.textContent = "Activate Stregsystemet";
     header.classList.add("activatefklub-header");
     document.body.querySelector(".activatefklub-container").appendChild(header);
 
     const description = document.createElement("p");
-    description.textContent = "Go to stregsystemet to pay for membership.";
+    description.textContent = "Go to the Admin panel to activate Stregsystemet.";
     document.body.querySelector(".activatefklub-container").appendChild(description);
 }
