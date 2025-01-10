c9b54c01fce4f911112147d5a28b87bbc48b440f
Kresten Laust <krestenlaust@gmail.com>
2025-01-10 17:50:35 +0100

    Refactor code

--- openapi/dredd_hook.py
+++ openapi/dredd_hook.py
@@ -2,9 +2,9 @@ import dredd_hooks as hooks
 import json
 
 not_found_parameter_values = {
-  'room_id': [1],
-  'member_id': [1],
-  'username': ["404_user"],
+  'room_id': 1,
+  'member_id': 1,
+  'username': "404_user",
 }
 
 skipped_endpoints = [
@@ -18,14 +18,23 @@ def skip_endpoint(transaction):
     transaction['skip'] = True
 
 
+def update_replace_dict(original_dict: dict, replacement: dict) -> None:
+  """
+  Same as dict.update(...) but doesn't add new values.
+  Modifies existing dict.
+  """
+  original_dict.update({k: v for k, v in replacement.items() if k in original_dict})
+
+
 def update_get_parameters(url_string: str, update_parameters: dict) -> str:
   from urllib.parse import parse_qs, urlencode, urlparse, urlunparse
 
   parsed_url = urlparse(url_string)
 
   qs_dict = parse_qs(parsed_url.query, keep_blank_values=True)
-  qs_dict.update({k: v for k, v in update_parameters.items() if k in qs_dict})
-  updated_qs = urlencode(qs_dict, doseq=True)
+  qs_dict_flattened = {key: value[0] for key, value in qs_dict.items()}
+  update_replace_dict(qs_dict_flattened, update_parameters)
+  updated_qs = urlencode(qs_dict_flattened, doseq=False)
 
   parsed_url = parsed_url._replace(query=updated_qs)
 
@@ -50,6 +59,6 @@ def replace_4xx_parameter_values(transaction):
 def replace_body_in_post_requests(transaction):
   if transaction['expected']['statusCode'][0] == '4' and transaction['id'].startswith("POST"):
     body = json.loads(transaction['request']['body'])
-    body['member_id'] = 1
+    update_replace_dict(body, not_found_parameter_values)
 
     transaction['request']['body'] = json.dumps(body)
