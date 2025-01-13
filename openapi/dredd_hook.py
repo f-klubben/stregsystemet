1b3f7f645eb7ce0a35ac2792dce4002db941c6a8
Kresten Laust <krestenlaust@gmail.com>
2025-01-13 18:41:33 +0100

    Reformat with 4 spaces instead of 2

--- openapi/dredd_hook.py
+++ openapi/dredd_hook.py
@@ -3,40 +3,40 @@ import json
 from utils import *
 
 not_found_parameter_values = {
-  'room_id': 1,
-  'member_id': 1,
-  'username': "404_user",
+    'room_id': 1,
+    'member_id': 1,
+    'username': "404_user",
 }
 
 skipped_endpoints = [
-  "GET (400) /api/member/payment/qr?username=kresten" # Skipped: test can't be implemented properly in OpenAPI
+    "GET (400) /api/member/payment/qr?username=kresten" # Skipped: test can't be implemented properly in OpenAPI
 ]
 
 @hooks.before_each
 def skip_endpoint(transaction):
-  if transaction['id'] in skipped_endpoints:
-    print(f"Skipping endpoint: {transaction['id']}")
-    transaction['skip'] = True
+    if transaction['id'] in skipped_endpoints:
+        print(f"Skipping endpoint: {transaction['id']}")
+        transaction['skip'] = True
 
 
 # https://dredd.org/en/latest/data-structures.html#transaction-object
 @hooks.before_each
 def replace_4xx_parameter_values(transaction):
-  """
-  It isn't possible to specify individual parameter example values for each response type in OpenAPI.
-  To properly test the return value of not-found parameters, replace all parameters.
-  """
-  if transaction['expected']['statusCode'][0] == '4':
-    new_path = update_get_parameters(transaction['fullPath'], not_found_parameter_values)
-    print(f"Update endpoint path, from '{transaction['fullPath']}' to '{new_path}'")
-    transaction['fullPath'] = new_path
-    transaction['request']['uri'] = new_path
+    """
+    It isn't possible to specify individual parameter example values for each response type in OpenAPI.
+    To properly test the return value of not-found parameters, replace all parameters.
+    """
+    if transaction['expected']['statusCode'][0] == '4':
+        new_path = update_get_parameters(transaction['fullPath'], not_found_parameter_values)
+        print(f"Update endpoint path, from '{transaction['fullPath']}' to '{new_path}'")
+        transaction['fullPath'] = new_path
+        transaction['request']['uri'] = new_path
 
 
 @hooks.before_each
 def replace_body_in_post_requests(transaction):
-  if transaction['expected']['statusCode'][0] == '4' and transaction['id'].startswith("POST"):
-    body = json.loads(transaction['request']['body'])
-    update_replace_dict(body, not_found_parameter_values)
+    if transaction['expected']['statusCode'][0] == '4' and transaction['id'].startswith("POST"):
+        body = json.loads(transaction['request']['body'])
+        update_replace_dict(body, not_found_parameter_values)
 
-    transaction['request']['body'] = json.dumps(body)
+        transaction['request']['body'] = json.dumps(body)
