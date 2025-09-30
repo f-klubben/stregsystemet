107921a79e34b80b486621fc32613a4b08ff92c4
Kresten Laust <krestenlaust@gmail.com>
2025-09-30 16:09:40 +0200

    Add website support to kiosk (#583)
    * Add website support to kiosk
    
    Kiosk now supports showing websites
    
    * fixup! Add website support to kiosk

--- kiosk/models.py
+++ kiosk/models.py
@@ -14,10 +14,15 @@ class KioskItem(models.Model):
     notes = models.CharField(max_length=2000, blank=True, null=True)
     uploaded_date = models.DateField(auto_now_add=True)
     active = models.BooleanField(default=True)
-    media = models.FileField(upload_to='kiosk', null=False, validators=[validate_file_extension])
+    media = models.FileField(upload_to='kiosk', blank=True, validators=[validate_file_extension])
+    website_url = models.URLField(blank=True)
     ordering = models.IntegerField(null=False, default=random_ordering, blank=False)
     start_datetime = models.DateTimeField(null=True, blank=True)
     end_datetime = models.DateTimeField(null=True, blank=True)
+
+    @property
+    def has_media(self):
+        return bool(self.media)
 
     @property
     def is_image(self):
