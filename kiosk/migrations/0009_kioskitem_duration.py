e8be755b0617322a3973db4ec21d46d2f053f7e5
Patrick <patrick@reiffenste.in>
2025-10-08 13:09:22 +0200

    Fix migration
    Since there have been other kiosk migration on next, this migration
    needed to be updated.

--- kiosk/migrations/0009_kioskitem_duration.py
+++ kiosk/migrations/0009_kioskitem_duration.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("kiosk", "0007_auto_20230823_1138"),
+        ("kiosk", "0008_kioskitem_website_url_alter_kioskitem_media"),
     ]
 
     operations = [
