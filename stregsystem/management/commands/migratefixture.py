9019a1c139f98c7d17d68f051ba5b832a593fae6
Kresten Laust <krestenlaust@gmail.com>
2026-04-03 22:53:12 +0200

    Add temp database instead of re-use default database

--- stregsystem/management/commands/migratefixture.py
+++ stregsystem/management/commands/migratefixture.py
@@ -1,6 +1,10 @@
 import tomllib
 from django.core.management import BaseCommand, call_command
+from django.db import connections
+from django.conf import settings
 from pathlib import Path
+import tempfile
+import os
 
 
 class Command(BaseCommand):
@@ -10,36 +14,59 @@ class Command(BaseCommand):
         parser.add_argument("--fixture", required=True)
 
     def handle(self, *args, **opts):
-        fixture = opts["fixture"]
+        tmp = tempfile.NamedTemporaryFile(suffix=".sqlite3", delete=False)
+        tmp.close()
 
-        targets = self.load_targets()
+        settings.DATABASES["default"] = {
+            **settings.DATABASES["default"],
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": tmp.name,
+        }
 
-        self.stdout.write("Migrating to targets")
+        try:
+            fixture = opts["fixture"]
+            targets = self.load_targets()
 
-        for app, migration in targets.items():
-            self.stdout.write(f"{app} -> {migration}")
-            call_command("migrate", app, migration)
+            self.stdout.write("Migrate all apps to latest")
+            call_command("migrate")
 
-        self.stdout.write("Dumping data")
+            self.stdout.write("Down-migrating to targets in pyproject.toml")
+            for app, migration in targets.items():
+                self.stdout.write(f"{app} -> {migration}")
+                call_command("migrate", app, migration)
 
-        with open(fixture, "w") as f:
-            call_command(
-                "dumpdata",
-                exclude=[
-                    "auth.permission",
-                    "contenttypes",
-                    "admin.logentry",
-                    "sessions",
-                ],
-                natural_foreign=True,
-                natural_primary=True,
-                indent=2,
-                stdout=f,
+            self.stdout.write("Load fixture data")
+            call_command("loaddata", fixture)
+
+            self.stdout.write("Re-migrate to targets on pyproject.toml")
+            for app, _ in targets.items():
+                self.stdout.write(f"{app}")
+                call_command("migrate", app)
+
+            self.stdout.write("Dumping fixtures")
+            with open(fixture, "w") as f:
+                call_command(
+                    "dumpdata",
+                    exclude=[
+                        "auth.permission",
+                        "contenttypes",
+                        "admin.logentry",
+                        "sessions",
+                    ],
+                    natural_foreign=True,
+                    natural_primary=True,
+                    indent=2,
+                    stdout=f,
+                )
+
+            self.stdout.write(
+                self.style.SUCCESS('Finished migrating fixtures')
             )
+        finally:
+            connections["default"].close()
+            del settings.DATABASES["_fixture_migration"]
+            os.unlink(tmp.name)
 
-        self.stdout.write(
-            self.style.SUCCESS('Successfully migrated fixture')
-        )
 
     def load_targets(self):
         with open("pyproject.toml", "rb") as f:
