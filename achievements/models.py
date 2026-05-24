b5a0f39dfe4d3590276bb44a077ddfd08c9f9b9d
Kresten Laust <krestenlaust@gmail.com>
2026-05-24 19:36:58 +0200

    Fix typo

--- achievements/models.py
+++ achievements/models.py
@@ -166,7 +166,7 @@ class AchievementConstraint(BaseModel):
 
     MONTHS = [
         (1, "January"),
-        (2, "Feburary"),
+        (2, "February"),
         (3, "March"),
         (4, "April"),
         (5, "May"),
