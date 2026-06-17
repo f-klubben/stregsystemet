de1198994d45834819c52f51b489c897e4576755
Kresten Laust <krestenlaust@gmail.com>
2026-06-17 02:21:32 +0200

    Update stregsystem/management/commands/migratefixture.py

--- stregsystem/management/commands/migratefixture.py
+++ stregsystem/management/commands/migratefixture.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("--fixture", required=False)
-        parser.add_argument("--all", action="store_true", default=False)
+        parser.add_argument("--all-fixtures", action="store_true", default=False)
         parser.add_argument("--update-targets", action="store_true", default=False)
 
     def handle(self, *args, **opts):
