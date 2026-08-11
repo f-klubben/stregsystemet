5e5e6067286844f0ea4f8c68990d33f563ff572c
Kresten Laust <krestenlaust@gmail.com>
2026-08-11 20:43:35 +0200

    Correct model to not include 'F' in code

--- sso/models.py
+++ sso/models.py
@@ -13,7 +13,7 @@ class MemberOTPRequest(models.Model):
     OTP_DIGITS = 5
 
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    code = models.CharField(max_length=OTP_DIGITS + 1)  #  An 'F'
+    code = models.CharField(max_length=OTP_DIGITS)  #  Only digits stored, but can start with multiple '0'
     failed_attempts = models.PositiveSmallIntegerField(default=0)
     is_valid = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
