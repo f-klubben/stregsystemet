4df4b20c7bc28b74af2b6f9d611dfe9a0f872f6c
Daniel Weper Jensen <danielweper@gmail.com>
2026-09-07 23:17:09 +0200

    Fixed linter issues

--- stregsystem/admin.py
+++ stregsystem/admin.py
@@ -208,9 +208,7 @@ class ProductAdmin(BaseAdmin):
 
 
 class NamedProductAdmin(BaseAdmin):
-    search_fields = (
-        'name',
-    )
+    search_fields = ('name',)
 
     def _get_fields_to_display(self):
         return [
