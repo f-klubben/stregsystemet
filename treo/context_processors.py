3c5c72225ba33b995c49d356529aa27c0caae319
Daniel Weper Jensen <danielweper@gmail.com>
2026-09-07 17:57:32 +0200

    Changed the signup to be exclusively JS

--- treo/context_processors.py
+++
@@ -1,9 +0,0 @@
-from django.utils import timezone
-
-def september_context(request):
-    month = timezone.now().month
-
-    is_september = (month == 9)
-    return {
-        "is_september": is_september,
-    }
